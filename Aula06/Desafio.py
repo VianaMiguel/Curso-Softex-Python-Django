ddd09b70f812b6890f6d2cc0e3306661b5fe3ffa
VianaMiguel <224689790+VianaMiguel@users.noreply.github.com>
2025-08-20 15:53:26 -0300

    Desenvolvimento de um aplicativo de Banco

---
+++ Aula06/Desafio.py
@@ -0,0 +1,55 @@
+""" Criação de um app bancario 
+1 - Rodar em loop infinito
+2 - Ter conta e senha (Validação)
+3 - Encerrar atendimento
+4 - Cheque Especial (Limitre de Saldo Negativo)
+5 - Ter 3 Tentativas para acertar a Senha
+6 - Opções (Saque, Deposito, Saldo)
+7 - Mostrar Saldo Após o Saque
+8 - Alterar Senha
+9 - Dizer o Nome do Usuario (Mensagem de Bem-Vindo)
+10 - Opção de Pagar Boleto
+"""
+# Declaração Constantes
+conta_corrente = "123456-7"
+senha_usuario = "9999"
+saldo_atual = 0
+limite_saldo_negativo = 500.00
+nome_usuario = "José"
+
+while True:
+    for i in range(3):
+        conta =input("Entre com a sua conta corrente: ")
+        senha =input("Entre com a sua senha")
+        if conta==conta_corrente and senha==senha_usuario:
+            print(f"Seja Bem-Vindo {nome_usuario}")
+            acesso_permitido=True
+            break
+        else:
+            print("Usuario ou Senha Invalidos")
+            acesso_permitido=False
+    if not acesso_permitido:
+        break
+
+    while True:
+        opcao = input("Escolha uma Opção:\n" \
+        "1 - Ver Saldo.\n" \
+        "2 - Sacar Valor.\n" \
+        "3 - Depositar.\n" \
+        "4 - Pagar Boleto.\n" \
+        "5 - Alterar Senha.\n" \
+        "6 - Sair.\n" \)
+        
+        if opcao =="1":
+            print(f"Saldo atual é de {saldo_atual}.")
+        elif opcao=="2":
+            valor_a_sacar= float(input("entre coom o valor a ser sacado: "))
+            if valor_a_sacar <= (saldo_atual + limite_saldo_negativo):
+                saldo -= valor_a_sacar
+                print("Saldo insuficiente!")
+        elif opcao=="3":
+            depositar = float(input("Insira o Valor a ser Depositado:"))
+            if depositar > 0:
+                saldo += despositar
+            else:
+                print("valor Inválido!")
