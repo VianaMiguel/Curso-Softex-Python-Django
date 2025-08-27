ef5bd7761c91c2b5eca901d3732a98581ec35342
VianaMiguel <224689790+VianaMiguel@users.noreply.github.com>
2025-08-27 15:43:23 -0300

    Exercicio03

---
+++ Aula08/Exercicio03.py
@@ -0,0 +1,10 @@
+"""Exercicio 3 -  Usando Replace"""
+
+frase= input("Digite sua frase: ")
+        
+
+codificada = frase.replace("a", "1").replace("e", "2").replace("i", "3").replace("o", "4").replace("u", "5") 
+print("Codificada: "+ codificada)
+
+decodificada = codificada.replace("1", "a").replace("2", "e").replace("3", "i").replace("4", "o").replace("5", "u") 
+print("Decodificada: "+ decodificada)
