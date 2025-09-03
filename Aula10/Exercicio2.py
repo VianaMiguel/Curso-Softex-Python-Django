ce460762743bb150317f7f68030db21ec3acce60
VianaMiguel <224689790+VianaMiguel@users.noreply.github.com>
2025-09-03 15:38:52 -0300

    Exercicio 2 Aula10

---
+++ Aula10/Exercicio2.py
@@ -0,0 +1,9 @@
+"""Set tranforma listas em conjuntos """
+estoque_principal=[ ] 
+estoque_online=[ ] 
+
+set_principal = set(estoque_principal)
+set_online = set( estoque_online)
+
+em_ambos = set_principal.intersection(set_online)
+
