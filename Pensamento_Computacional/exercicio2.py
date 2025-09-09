c9de7d8b1d882ca7533b0a8ba11c67a681d632b7
VianaMiguel <224689790+VianaMiguel@users.noreply.github.com>
2025-09-08 22:58:15 -0300

    Exercicio2

---
+++ Pensamento_Computacional/exercicio2.py
@@ -0,0 +1,11 @@
+"""Exercício 2: Crie um algoritmo para somar dois números e mostrar o resultado."""
+
+
+#Recebo o primeiro número a se somado:
+num_1=float(input("Digite um numero: "))
+#Recebo o segundo número a somar:
+num_2=float(input("Digite o numero a somar: "))
+#Calculo o resultado da soma:
+resultado=(num_1 +num_2)
+#Imprimo o resultado:
+print(resultado)
