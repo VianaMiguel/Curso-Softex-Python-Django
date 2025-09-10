cfb6564f7f62c778c329a1e2be6d095302d4d2e4
VianaMiguel <224689790+VianaMiguel@users.noreply.github.com>
2025-09-09 21:42:53 -0300

    Exercicio3

---
+++ Aula12/Exercicio3.py
@@ -0,0 +1,19 @@
+""" Exercicio 3
+
+Soma de Dois Números: Crie uma função que receba dois números e retorne a soma 
+deles."""
+
+def soma (a:int, b:int):
+    return a + b
+
+resultado = soma(11,12)
+print (resultado)
+
+"""ou"""
+
+
+def soma (a:int, b:int):
+    return a + b
+
+total:int = soma(11,12)
+print(f"Soma é: { total}")
