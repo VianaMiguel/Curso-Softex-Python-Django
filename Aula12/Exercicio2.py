2c834a823350e855f97c597e759ea13098c44f42
VianaMiguel <224689790+VianaMiguel@users.noreply.github.com>
2025-09-09 21:42:15 -0300

    Exercicio2

---
+++ Aula12/Exercicio2.py
@@ -0,0 +1,9 @@
+"""Exercicio 2
+
+Saudação Personalizada: Crie uma função que receba um nome como parâmetro e 
+imprima "Olá, [nome]!". """
+
+def saudacao(nome:str):
+    print(f"Olá, {nome} !")
+
+saudacao("Miguel")
