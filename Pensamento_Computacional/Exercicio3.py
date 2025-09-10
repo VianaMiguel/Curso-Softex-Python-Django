4a475e2f0313ef12e172846463320e303f944ac0
VianaMiguel <224689790+VianaMiguel@users.noreply.github.com>
2025-09-10 20:04:01 -0300

    Exercicio4

---
+++ Pensamento_Computacional/Exercicio3.py
@@ -0,0 +1,14 @@
+"""Exercício 3: 
+
+Crie um algoritmo que conte de 1 a 10 e imprima cada número na tela. Use um
+loop ENQUANTO."""
+
+
+#Inicializa o contador em 1:
+contador= 1
+#Enquanto contador for menor ou igual a 10:
+while contador <= 10:
+    #Imprime o valor atual do contador:
+    print(contador)
+    #Incrementa o contador em 1 até cumprir o while:
+    contador += 1
