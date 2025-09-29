16979fde7d226e3224060ab5bfac4890fd3610e4
VianaMiguel <224689790+VianaMiguel@users.noreply.github.com>
2025-09-29 15:58:46 -0300

    exercicio5

---
+++ Aula17/exercicio5.py
@@ -0,0 +1,14 @@
+class Comodo:
+    def __init__(self,nome):
+        self.nome = nome
+        print
+
+class Casa:
+    def __init__(self):
+        pass
+
+    def adicionar_comodo(self,nome):
+        self.nome = nome
+
+    def listar_comodos()
+        
