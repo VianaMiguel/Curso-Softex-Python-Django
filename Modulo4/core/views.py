782798ccb699fc7f354438c6d52d2b8ae6390878
VianaMiguel <224689790+VianaMiguel@users.noreply.github.com>
2025-11-05 15:38:46 -0300

    feat : Segunda aula de Django (aula 28)

--- Modulo4/core/views.py
+++ Modulo4/core/views.py
@@ -6,7 +6,15 @@ from django.http import HttpResponse
 # Uma 'view' é uma função que recebe um 'request' e retorna uma 'response'
 def home(request):
 # Vamos retornar a resposta HTTP mais simples: um texto HTML
-    return HttpResponse("<h1>Olá, Mundo! Esta é minha primeira página Django!</h1>")
+#return HttpResponse("<h1>Olá, Mundo! Esta é minha primeira página Django!</h1>") modo antigo
+
+# 1. Crie seu dicionário de contexto
+    context = {
+        'nome_usuario': 'Júnior',
+        'tecnologias': ['Python', 'Django', 'HTML', 'CSS']
+    }
+
+    return render(request, 'home.html', context)
 
 def saudacao(request):
-    return HttpResponse("<h1>Saudacao</h1>")
+    return HttpResponse("<input>Saudacao</input>")
