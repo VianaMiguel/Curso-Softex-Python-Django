d796455ba7bd721616145a431de311d5504f190c
VianaMiguel <224689790+VianaMiguel@users.noreply.github.com>
2025-11-10 16:03:36 -0300

    feat  Terceira aula de Django(Aula 29) Apostila 3

--- Modulo4/core/admin.py
+++ Modulo4/core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Tarefa # 1. Importe seu Model
 
 # Register your models here.
+
+# 2. "Registre" seu Model no site de administração
+admin.site.register(Tarefa)
+
+#Usuário : admin
+#Endereço : admin@admin.com
+#Senha: admin
--- Modulo4/core/views.py
+++ Modulo4/core/views.py
@@ -1,17 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Tarefa
 
 # Create your views here.
 
 # Uma 'view' é uma função que recebe um 'request' e retorna uma 'response'
 def home(request):
+# 2. Use o ORM para buscar os dados!
+# Tarefa.objects.all() significa: "Pegue todas as linhas da tabela Tarefa"
+    todas_as_tarefas = Tarefa.objects.all()
 # Vamos retornar a resposta HTTP mais simples: um texto HTML
 #return HttpResponse("<h1>Olá, Mundo! Esta é minha primeira página Django!</h1>") modo antigo
 
 # 1. Crie seu dicionário de contexto
     context = {
         'nome_usuario': 'Júnior',
-        'tecnologias': ['Python', 'Django', 'HTML', 'CSS']
+        'tecnologias': ['Python', 'Django', 'HTML', 'CSS','Models', 'Admin'],
+        'tarefas': todas_as_tarefas # 4. Adicione as tarefas ao contexto
     }
 
     return render(request, 'home.html', context)
